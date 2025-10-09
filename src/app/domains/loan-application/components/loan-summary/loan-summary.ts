dbfaca51d6915e143fc27d9929e274c8b2519f28
Andre McMorris <ajmcmorris@gmail.com>
2025-10-09 14:04:55 -0400

    feat: updated summary

--- src/app/domains/loan-application/components/loan-summary/loan-summary.ts
+++ src/app/domains/loan-application/components/loan-summary/loan-summary.ts
@@ -1,4 +1,4 @@
-import { Component, signal } from '@angular/core';
+import { Component, Input, signal } from '@angular/core';
 
 @Component({
   selector: 'app-loan-summary',
@@ -8,4 +8,7 @@ import { Component, signal } from '@angular/core';
 })
 export class LoanSummary {
   protected readonly main_header = signal('Loan Summary');
+  @Input() summary = signal(
+    'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
+  );
 }
