f22a9e487ebb3e930329fa3355cfa13f6437117f
Andre McMorris <ajmcmorris@gmail.com>
2025-10-11 22:37:13 -0400

    refactor: add base underwriting styles

--- src/app/domains/underwriting/components/underwriting-panel/underwriting-panel.ts
+++ src/app/domains/underwriting/components/underwriting-panel/underwriting-panel.ts
@@ -1,8 +1,10 @@
 import { Component } from '@angular/core';
+import { LoanDetails } from '../loan-details/loan-details';
+import { DecisionHistory } from '../decision-history/decision-history';
 
 @Component({
   selector: 'app-underwriting-panel',
-  imports: [],
+  imports: [LoanDetails, DecisionHistory],
   templateUrl: './underwriting-panel.html',
   styleUrl: './underwriting-panel.scss',
 })
