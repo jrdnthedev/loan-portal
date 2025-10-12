2fed5dd448e5677ae18ce432942f530439ecda9b
Andre McMorris <ajmcmorris@gmail.com>
2025-10-12 09:34:05 -0400

    refactor: added test data

--- src/app/domains/underwriting/components/loan-details/loan-details.ts
+++ src/app/domains/underwriting/components/loan-details/loan-details.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { LoanApplicationStore } from '../../../loan-application/store/loan-application.store';
 
 @Component({
   selector: 'app-loan-details',
@@ -6,4 +7,14 @@ import { Component } from '@angular/core';
   templateUrl: './loan-details.html',
   styleUrl: './loan-details.scss',
 })
-export class LoanDetails {}
+export class LoanDetails {
+  private store = inject(LoanApplicationStore);
+
+  readonly currentLoan$ = this.store.currentLoan$;
+  readonly state$ = this.store.state$;
+
+  ngOnInit() {
+    this.currentLoan$.subscribe((data) => console.log('currentLoan', data));
+    this.state$.subscribe((data) => console.log('hello state', data));
+  }
+}
