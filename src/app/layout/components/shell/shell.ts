2311cebc922fdd6c5cccfa9fa3022c3cad07c045
Andre McMorris <ajmcmorris@gmail.com>
2025-10-15 15:12:55 -0400

    refactor: added some links to navigate

--- src/app/layout/components/shell/shell.ts
+++ src/app/layout/components/shell/shell.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { Dashboard } from '../dashboard/dashboard';
 import { Card } from '../../../shared/components/card/card';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-shell',
-  imports: [Dashboard, Card],
+  imports: [Dashboard, Card, RouterLink],
   templateUrl: './shell.html',
   styleUrl: './shell.scss',
 })
