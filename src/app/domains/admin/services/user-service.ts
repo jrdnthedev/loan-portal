ad43dcc629cde2e737a326458c4cb28f1064b921
Andre McMorris <ajmcmorris@gmail.com>
2025-10-16 15:05:56 -0400

    feat: created user service

---
+++ src/app/domains/admin/services/user-service.ts
@@ -0,0 +1,21 @@
+import { Injectable } from '@angular/core';
+import { environment } from '../../../../environments/environment';
+import { Observable } from 'rxjs';
+import { HttpClient } from '@angular/common/http';
+import { User } from '../models/user';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class UserService {
+  private readonly apiUrl = environment.apiUrl;
+  constructor(private http: HttpClient) {}
+
+  getUsers(): Observable<User[]> {
+    return this.http.get<User[]>(`${this.apiUrl}/users`);
+  }
+
+  getUser(id: number): Observable<User> {
+    return this.http.get<User>(`${this.apiUrl}/users/${id}`);
+  }
+}
