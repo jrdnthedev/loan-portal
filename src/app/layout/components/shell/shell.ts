797813aa41e22cb8618ae912c58ca2bd34a82815
Andre McMorris <ajmcmorris@gmail.com>
2025-10-16 17:06:11 -0400

    refactor: updated shell to include table component

--- src/app/layout/components/shell/shell.ts
+++ src/app/layout/components/shell/shell.ts
@@ -1,12 +1,42 @@
-import { Component } from '@angular/core';
+import { Component, inject, signal } from '@angular/core';
 import { Dashboard } from '../dashboard/dashboard';
 import { Card } from '../../../shared/components/card/card';
 import { RouterLink } from '@angular/router';
+import { Table } from '../../../shared/components/table/table';
+import { UserService } from '../../../domains/admin/services/user-service';
+import { User } from '../../../domains/admin/models/user';
+import { catchError, map } from 'rxjs';
 
 @Component({
   selector: 'app-shell',
-  imports: [Dashboard, Card, RouterLink],
+  imports: [Dashboard, Card, RouterLink, Table],
   templateUrl: './shell.html',
   styleUrl: './shell.scss',
 })
-export class Shell {}
+export class Shell {
+  private userService = inject(UserService);
+  userData = signal<Partial<User>[]>([]);
+
+  ngOnInit() {
+    this.userService
+      .getUsers()
+      .pipe(
+        catchError((err: Error) => {
+          console.error(err);
+          return [];
+        }),
+        map((users: User[]) =>
+          users.map((user: User) => {
+            const { profile, ...userProps } = user;
+            return {
+              ...userProps,
+              ...profile,
+            };
+          }),
+        ),
+      )
+      .subscribe((users: Partial<User>[]) => {
+        this.userData.set(users);
+      });
+  }
+}
