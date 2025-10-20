3e7b85a4f3a8ff94dd61b9855b28d928a2767c9c
Andre McMorris <ajmcmorris@gmail.com>
2025-10-20 12:35:36 -0400

    feat: created loan officer component

---
+++ src/app/domains/loan-application/components/loan-officer/loan-officer.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-loan-officer',
+  imports: [],
+  templateUrl: './loan-officer.html',
+  styleUrl: './loan-officer.scss',
+})
+export class LoanOfficer {}
