7d03ae618a4e8c9a29346d5553deedf34bc435a3
Andre McMorris <ajmcmorris@gmail.com>
2025-10-22 11:21:03 -0400

    refactor: moved loan officer logic into parent component

--- src/app/domains/loan-application/components/loan-officer/loan-officer.ts
+++ src/app/domains/loan-application/components/loan-officer/loan-officer.ts
@@ -1,19 +1,12 @@
-import { Component, inject } from '@angular/core';
-import { AdminStore } from '../../../admin/store/admin.store';
-import { UserRole } from '../../../admin/models/user-role';
-import { AsyncPipe } from '@angular/common';
+import { Component, Input } from '@angular/core';
+import { User } from '../../../admin/models/user';
 
 @Component({
   selector: 'app-loan-officer',
-  imports: [AsyncPipe],
+  imports: [],
   templateUrl: './loan-officer.html',
   styleUrl: './loan-officer.scss',
 })
 export class LoanOfficer {
-  private store$ = inject(AdminStore);
-  filteredUsers$ = this.store$.filteredUsers$;
-
-  ngOnInit() {
-    this.store$.filterUsersByType('loan-officer');
-  }
+  @Input({ required: true }) loanOfficer: User[] = [];
 }
--- src/app/layout/components/dashboard/dashboard.ts
+++ src/app/layout/components/dashboard/dashboard.ts
@@ -1,23 +1,29 @@
 import { Component, inject } from '@angular/core';
-import { CommonModule } from '@angular/common';
+import { AsyncPipe } from '@angular/common';
 import { map } from 'rxjs';
 import { Card } from '../../../shared/components/card/card';
 import { PendingApplication } from '../../../domains/loan-application/components/pending-application/pending-application';
 import { LoanOfficer } from '../../../domains/loan-application/components/loan-officer/loan-officer';
 import { LoanApplicationStore } from '../../../domains/loan-application/store/loan-application.store';
 import { Loan } from '../../../domains/loan-application/models/loan';
+import { AdminStore } from '../../../domains/admin/store/admin.store';
 
 @Component({
   selector: 'app-dashboard',
-  imports: [CommonModule, Card, PendingApplication, LoanOfficer],
+  imports: [AsyncPipe, Card, PendingApplication, LoanOfficer],
   templateUrl: './dashboard.html',
   styleUrl: './dashboard.scss',
 })
 export class Dashboard {
   private readonly loanStore = inject(LoanApplicationStore);
-
+  private readonly adminStore = inject(AdminStore);
   // Get the last 3 loans for the pending applications display
   recentLoans$ = this.loanStore.userLoans$.pipe(
     map((loans: Loan[]) => loans.slice(Math.max(0, loans.length - 3))),
   );
+  filteredUsers$ = this.adminStore.filteredUsers$;
+
+  ngOnInit() {
+    this.adminStore.filterUsersByType('loan-officer');
+  }
 }
