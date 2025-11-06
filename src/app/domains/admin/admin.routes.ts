e9e18f3c65e18b298a74fcbd132467c01e082c21
Andre McMorris <ajmcmorris@gmail.com>
2025-11-06 14:40:35 -0500

    refactor: updated routes files

--- src/app/domains/admin/admin.routes.ts
+++ src/app/domains/admin/admin.routes.ts
@@ -7,20 +7,20 @@ export const ADMIN_ROUTES: Routes = [
     pathMatch: 'full',
   },
   {
-    path: 'audit_log',
-    loadComponent: () => import('./components/audit-log/audit-log').then((m) => m.AuditLog),
-    title: 'Audit Log',
-  },
-  {
     path: 'admin_console',
     loadComponent: () =>
       import('./components/admin-console/admin-console').then((m) => m.AdminConsole),
     title: 'Admin Console',
-  },
-  {
-    path: 'user_management',
-    loadComponent: () =>
-      import('./components/user-management/user-management').then((m) => m.UserManagement),
-    title: 'User Management',
+    children: [
+      {
+        path: 'audit_log',
+        loadComponent: () => import('./components/audit-log/audit-log').then((m) => m.AuditLog),
+      },
+      {
+        path: 'user_management',
+        loadComponent: () =>
+          import('./components/user-management/user-management').then((m) => m.UserManagement),
+      },
+    ],
   },
 ];
--- src/app/layout/layout.routes.ts
+++ src/app/layout/layout.routes.ts
@@ -16,14 +16,4 @@ export const LAYOUT_ROUTES: Routes = [
     loadComponent: () => import('./components/dashboard/dashboard').then((m) => m.Dashboard),
     title: 'Dashboard',
   },
-  {
-    path: 'nav',
-    loadComponent: () => import('./components/nav/nav').then((m) => m.Nav),
-    title: 'Navigation Bar',
-  },
-  {
-    path: 'sidepanel',
-    loadComponent: () => import('./components/sidepanel/sidepanel').then((m) => m.Sidepanel),
-    title: 'Side Panel',
-  },
 ];
