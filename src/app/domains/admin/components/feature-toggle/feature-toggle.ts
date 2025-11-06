7759a7aae2f50d129d1f9a2f90de7f7c285c34f6
Andre McMorris <ajmcmorris@gmail.com>
2025-11-06 14:18:51 -0500

    feat: added feature toggle component

---
+++ src/app/domains/admin/components/feature-toggle/feature-toggle.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-feature-toggle',
+  imports: [],
+  templateUrl: './feature-toggle.html',
+  styleUrl: './feature-toggle.scss',
+})
+export class FeatureToggle {}
