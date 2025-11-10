9d1af0d699c333e2d72abf5cfddeb20f51c782cc
Andre McMorris <ajmcmorris@gmail.com>
2025-11-10 15:08:32 -0500

    refactor: added audit logs to component

--- src/app/domains/admin/components/audit-log/audit-log.ts
+++ src/app/domains/admin/components/audit-log/audit-log.ts
@@ -1,9 +1,14 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { AdminStore } from '../../store/admin.store';
+import { AsyncPipe } from '@angular/common';
 
 @Component({
   selector: 'app-audit-log',
-  imports: [],
+  imports: [AsyncPipe],
   templateUrl: './audit-log.html',
   styleUrl: './audit-log.scss',
 })
-export class AuditLog {}
+export class AuditLog {
+  private store = inject(AdminStore);
+  readonly auditLogs$ = this.store.auditLogs$;
+}
