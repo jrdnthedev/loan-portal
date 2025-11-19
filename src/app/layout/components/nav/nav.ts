6c04323123d774ea521bd8aeb3285945567d3db8
Andre McMorris <ajmcmorris@gmail.com>
2025-11-19 15:07:53 -0500

    refactor: added auth checks and modal dialogs for auth actions

--- src/app/layout/components/nav/nav.ts
+++ src/app/layout/components/nav/nav.ts
@@ -1,14 +1,21 @@
-import { Component, signal } from '@angular/core';
+import { Component, inject, signal } from '@angular/core';
 import { RouterLink } from '@angular/router';
+import { AuthService } from '../../../core';
+import { Login } from '../../../core/auth/components/login/login';
+import { Register } from '../../../core/auth/components/register/register';
+import { Modal } from '../../../shared/components/modal/modal';
 
 @Component({
   selector: 'app-nav',
-  imports: [RouterLink],
+  imports: [RouterLink, Login, Register, Modal],
   templateUrl: './nav.html',
   styleUrl: './nav.scss',
 })
 export class Nav {
   isMenuOpen = signal(false);
+  authService = inject(AuthService);
+  isLoginModalOpen = signal(false);
+  isRegisterModalOpen = signal(false);
 
   toggleMenu() {
     this.isMenuOpen.update((isOpen) => !isOpen);
@@ -16,5 +23,13 @@ export class Nav {
 
   closeMenu() {
     this.isMenuOpen.set(false);
+  }
+
+  toggleLogin() {
+    this.isLoginModalOpen.update((value) => !value);
+  }
+
+  toggleRegister() {
+    this.isRegisterModalOpen.update((value) => !value);
   }
 }
