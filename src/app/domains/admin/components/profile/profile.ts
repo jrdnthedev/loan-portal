d03ede4c9cffff00be0c9e2c41f53b03fc35f56e
Andre McMorris <ajmcmorris@gmail.com>
2025-11-26 12:17:27 -0500

    refactor: added new styles to profile component

--- src/app/domains/admin/components/profile/profile.ts
+++ src/app/domains/admin/components/profile/profile.ts
@@ -1,9 +1,10 @@
 import { Component, inject } from '@angular/core';
 import { AuthService } from '../../../../core/auth/services/auth.service';
+import { Card } from '../../../../shared/components/card/card';
 
 @Component({
   selector: 'app-profile',
-  imports: [],
+  imports: [Card],
   templateUrl: './profile.html',
   styleUrl: './profile.scss',
 })
