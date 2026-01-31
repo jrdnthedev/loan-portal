9c3eafbe9b6ddb44521a88610ca6185f97cef080
Andre McMorris <ajmcmorris@gmail.com>
2026-01-31 14:02:09 -0500

    feat: added new badge component

---
+++ src/app/shared/components/badge/badge.ts
@@ -0,0 +1,11 @@
+import { Component, Input } from '@angular/core';
+
+@Component({
+  selector: 'app-badge',
+  imports: [],
+  templateUrl: './badge.html',
+  styleUrl: './badge.scss',
+})
+export class Badge {
+  @Input() title = '';
+}
